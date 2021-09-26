32a564afa332f37981f5588176426e6777c6c644
Matheus.R <matheus.rlank2@gmail.com>
2021-09-26 19:03:26 -0300

    Menu 100%

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 function Header() {
+
+    const [menu,setMenu] = useState(false)
+
     return (
         <section className="header">
             <div className="logo">
                 <img src="./images/logo-corebiz-global.svg" alt="logo-corebiz"></img>
             </div>
-            <div>
-            <div class="menu-toggle">
+            <div className={ menu ? "menu-section on": "menu-section" }>
+              <div onClick={()=> setMenu(!menu) } className="menu-toggle">
                 <div class="one"></div>
                 <div class="two"></div>
                 <div class="three" ></div>
             </div>
 
-            <div className="menu">
+            <nav className="menu">
                 <ul>
                     <li><a className="link" target="_blank" rel="noreferrer" href="https://www.corebiz.ag/pt/about/">a corebiz</a></li>
                     <li><a className="link" target="_blank" rel="noreferrer" href="https://www.corebiz.ag/pt/#framework-title">serviços</a></li>  
                     <li><a className="link" target="_blank" rel="noreferrer" href="https://www.corebiz.ag/pt/cases/">cases</a></li>
                     <li><a className="link" target="_blank" rel="noreferrer" href="https://www.corebiz.ag/pt/contato/">contato</a></li>
                 </ul>
-            </div>
+            </nav>
             </div>
         </section>
     )
